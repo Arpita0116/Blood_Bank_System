5049b374074a56abbec89c85b62c2747ecb1a2c3
Arpita0116 <166647545+ArpitaPatel15@users.noreply.github.com>
2024-07-09 07:09:25 +0530

    my 1st commit

---
+++ server.js
@@ -0,0 +1,21 @@
+let express = require('express')
+let app = express()
+let dotenv = require('dotenv')
+let cors = require('cors')
+let colors = require('colors')
+let morgan = require('morgan')
+const databaseConnection = require('./config/db')
+const route = require('./routes/auth')
+const inventoryRoute = require('./routes/inventroyRoute')
+dotenv.config()
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+databaseConnection()
+
+app.use('/auth/v1', route)
+app.use('/inventory/v1', inventoryRoute)
+let PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is Running At ${PORT}`.bgGreen.white);
+})
