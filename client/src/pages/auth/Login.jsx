f4f9e4320f3471ee67df5ee2a5e8d931926bd927
Arpita0116 <166647545+ArpitaPatel15@users.noreply.github.com>
2024-07-10 06:08:02 +0530

    layout setup

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import Form from '../../component/shared/Form/Form'
+import React, { lazy, Suspense } from 'react'
+
+const Form = lazy(() => import('../../component/shared/Form/Form'))
+
 function Login() {
     return (
         <>
             <div className='row g-0'>
                 <div className='col-md-5'>
-                    <img src='assest/BloodBank.jpg' alt='Blood Bank Login' className='img-fluid login-image' loading='lazy' />
+                    {/* <image src='./assest/BloodBank.jpg' alt='Blood Bank Login' className='img-fluid login-image' /> */}
+                    <img src='./assest/BloodBank.jpg' alt='Blood Bank Login' className='img-fluid login-image' />
                 </div>
-                <div className='col-md-6 form-container'>
-
-                    <Form role="" submitText="submit" formTitle="Login" formType="login" />
+                <div className='col-md-6 from-container'>
+                    <Suspense fallback={<div>Loading.....</div>}>
+                        <Form role="" submitText="submit" formTitle="Login" formType="login" />
+                    </Suspense>
 
                 </div>
+
             </div>
         </>
-    );
+    )
 }
 
-export default Login;
+export default Login
--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Form from '../../component/shared/Form/Form'
+import React, { lazy, Suspense } from 'react'
+const Form = lazy(() => import('../../component/shared/Form/Form'))
 function Register() {
     return (
         <>
@@ -8,7 +8,9 @@ function Register() {
                     <img src='./assest/BloodRegister.jpg' alt="blood_bank_login" className='img-fluid login-image' />
                 </div>
                 <div className='col-md-6 form-container'>
-                    <Form submitText="Register" formTitle="Register" formType="register" />
+                    <Suspense fallback={<div>Loading.....</div>}>
+                        <Form submitText="Register" formTitle="Register" formType="register" />
+                    </Suspense>
                 </div>
             </div>
         </>
